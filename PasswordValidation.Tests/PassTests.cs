ea3e989fc3e378d61bd67e6f119caef9b47f603f
Anastasia Ovtsunova <ovtsunova@hotmail.com>
2024-09-09 21:29:42 +0300

    myaw

---
+++ PasswordValidation.Tests/PassTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PasswordValidation.Tests
+{
+    [TestClass]
+    public class PassTests
+    {
+        [TestMethod]
+        public void Valid_EmptyPass_0return()
+        {
+            int mark = Pass.Valid("");
+            Assert.AreEqual(0, mark);
+        }
+        [TestMethod]
+        public void Valid_DigitChar_1return()
+        {
+            int mark = Pass.Valid("123193");
+            Assert.AreEqual(1, mark);
+        }
+        [TestMethod]
+        public void Valid_LowerChar_2return()
+        {
+            int mark = Pass.Valid("d00phakz");
+            Assert.AreEqual(2, mark);
+        }
+        [TestMethod]
+        public void Valid_UpperChar_3return()
+        {
+            int mark = Pass.Valid("D00PhaKz");
+            Assert.AreEqual(3, mark);
+        }
+        [TestMethod]
+        public void Valid_SpecialChar_4return()
+        {
+            int mark = Pass.Valid("D00Ph@Kz");
+            Assert.AreEqual(4, mark);
+        }
+        [TestMethod]
+        public void Valid_LengthCheck_5return()
+        {
+            int mark = Pass.Valid("D00Ph@Kzzzz");
+            Assert.AreEqual(5, mark);
+        }
+    }
+}
---
+++ PasswordValidation/Pass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordValidation
+{
+    public class Pass
+    {
+        public static int Valid(string pass)
+        {
+            if (string.IsNullOrEmpty(pass))
+                return 0;
+
+            int mark = 0;
+
+            if (pass.Any(char.IsDigit)) mark++;
+            if (pass.Any(char.IsLower)) mark++;
+            if (pass.Any(char.IsUpper)) mark++;
+            if (Regex.IsMatch(pass, @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]")) mark++;
+            if (pass.Length > 10) mark++;
+
+            return mark;
+        }
+    }
+}
